9caad58d72589be30f462ccaf8980a39cb62e4bd
Elena Ishmukhametova <renbiwlen@gmail.com>
2025-05-29 19:03:13 +0300

    Изменен нейминг переменных

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 function createCard(card) {
-  const cardNew = document.querySelector('#card-template').content;
-  const cardTemplate = cardNew.querySelector('.card').cloneNode(true);
-  cardTemplate.querySelector('.card__image').src = card.link;
-  cardTemplate.querySelector('.card__description').querySelector('.card__title').textContent = card.name;
+  const cardNew = cardTemplate.querySelector(".card").cloneNode(true);
+  cardNew.querySelector(".card__image").src = card.link;
+  cardNew.querySelector(".card__description").querySelector(".card__title").textContent = card.name;
 
-  const buttonDelete = cardTemplate.querySelector('.card__delete-button');
-  buttonDelete.addEventListener('click', () => deleteCard(cardTemplate));
+  const buttonDelete = cardNew.querySelector(".card__delete-button");
+  buttonDelete.addEventListener("click", () => deleteCard(cardNew));
 
-  return cardTemplate;
+  return cardNew;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(delcard) {
-  delcard.remove();
+function deleteCard(delCard) {
+  delCard.remove();
 }
 
 // @todo: Вывести карточки на страницу
-const massive = document.querySelector('.places__list');
-for (let i=0; i < initialCards.length; i++) {
-  massive.append(createCard(initialCards[i]));
+const cardsList = document.querySelector(".places__list");
+for (let i = 0; i < initialCards.length; i++) {
+  cardsList.append(createCard(initialCards[i]));
 }
