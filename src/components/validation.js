b621a5ed2113710f67784a1e7da10406d0872f19
Elena Ishmukhametova <renbiwlen@gmail.com>
2025-07-17 00:16:48 +0300

    Добавлена интеграция с API

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,32 +1,93 @@
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
 // Валидация формы
-function enableValidation(config) {
+export function enableValidation(config) {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
-  const inputList = Array.from(formList.querySelectorAll(config.inputSelector));
-  inputList.forEach((inputElement)=> {
-    inputElement.addEventListener("input", isValid)
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, config);
   })
 }
 
-// очистка формы
-function clearValidation(profileForm, validationConfig){
+// обработчик событий ввода в инпут 
+export function setEventListeners(formElement, config) {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
 
+  toggleButtonState(inputList, buttonElement, config);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement, config);
+      toggleButtonState(inputList, buttonElement, config);
+    });
+  });
+}; 
+
+// очистка формы
+export function clearValidation(formElement, config){
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  inputList.forEach((inputElement)=> {
+    hideInputError(formElement, inputElement, config);
+  })
+  toggleButtonState(inputList, buttonElement, config); 
 }
 
+// проверка на валидность всех инпутов
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+
+//переключатель кнопки
+const toggleButtonState = (inputList, buttonElement, config) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.disabled = true;
+    buttonElement.classList.add(config.inactiveButtonClass);
+  } else {
+    // иначе сделай кнопку активной
+    buttonElement.disabled = false;
+    buttonElement.classList.remove(config.inactiveButtonClass);
+  }
+};
+
 //присвоить класс ошибки
-function showInputError(elem) {
-  elem.classList.add(config.inputErrorClass);
+export function showInputError(formElement, inputElement, errorMessage, config) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
 }
 
 //скрыть класс ошибки
-function hideInputError(elem) {
-  elem.classList.remove(config.inputErrorClass);
+export function hideInputError(formElement, inputElement, config) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.classList.remove(config.errorClass);
+  errorElement.textContent = "";
 }
 
-function isValid(elem) {
-  if (!elem.validity.valid) {
-    showInputError(elem)
+export function isValid(formElement, inputElement, config) {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
   }
-  else {
-    hideInputError(elem)
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, config);
+  } else {
+    hideInputError(formElement, inputElement, config);
   }
 }
--- src/index.js
+++ src/index.js
@@ -23,18 +23,42 @@ const imageName = popupImage.querySelector(".popup__caption");
 
 const profileName = document.querySelector(".profile__title");
 const profileDescrip = document.querySelector(".profile__description");
+const profileAvatar = document.querySelector(".profile__image");
+
 
 // @todo: Вывести карточки на страницу
 const cardsList = document.querySelector(".places__list");
-initialCards.forEach((card) => {
-  cardsList.append(createCard(card, deleteCard, dataCardImg, imageLiked));
-});
+//initialCards.forEach((card) => {
+ // cardsList.append(createCard(card, deleteCard, dataCardImg, imageLiked));
+//});
+
+function getCards() {
+  fetch('https://nomoreparties.co/v1/wff-cohort-42/cards', {
+    headers: {
+      authorization: '4baeca36-0e67-4bf5-a839-8080e6cb541f'
+    }
+  })
+    .then(res => res.json())
+    .then((result) => {
+      result.forEach((card)=>{
+        cardsList.append(createCard(card, deleteCard, dataCardImg, imageLiked));
+      })
+    });
+}
 
 //открытие формы редактирования профиля, инпуты заполняются данными профиля
 buttonEdit.addEventListener("click", function (evt) {
   document.forms.editprofile.name.value = profileName.textContent;
   document.forms.editprofile.description.value = profileDescrip.textContent;
   openModal(popupEdit);
+  clearValidation(document.querySelector('.popup__form'),{
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
 });
 
 // попап фулвью заполняется данными изображения по клику, открытие попапа
@@ -73,6 +97,7 @@ function handleFormEdit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescrip.textContent = jobInput.value;
+  patchUser();
   closeModal(popupEdit);
 }
 
@@ -83,17 +108,28 @@ formEdit.addEventListener("submit", handleFormEdit);
 const formCard = document
   .querySelector(".popup_type_new-card")
   .querySelector(".popup__form");
+const cardName = formCard.querySelector(".popup__input_type_card-name");
+const cardLink = formCard.querySelector(".popup__input_type_url");
 let cardForm = {};
 
 // обработчик создания новых карточек
 function handleNewCard(evt) {
   evt.preventDefault();
-  cardForm.name = formCard.querySelector(".popup__input_type_card-name").value;
-  cardForm.link = formCard.querySelector(".popup__input_type_url").value;
+  cardForm.name = cardName.value;
+  cardForm.link = cardLink.value;
   const cardModal = createCard(cardForm, deleteCard, dataCardImg, imageLiked);
   cardsList.prepend(cardModal);
+  postCard();
   // Очистка инпутов
   formCard.reset();
+  clearValidation(document.querySelector('.popup__form'),{
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
   closeModal(popupNewCard);
 }
 
@@ -109,3 +145,53 @@ enableValidation({
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 }); 
+
+
+// заполнение профиля данными из запроса
+function getUser() {
+fetch('https://nomoreparties.co/v1/wff-cohort-42/users/me', {
+  headers: {
+    authorization: '4baeca36-0e67-4bf5-a839-8080e6cb541f'
+  }
+})
+  .then(res => res.json())
+  .then((result) => {
+    profileName.textContent = result.name;
+    profileDescrip.textContent = result.about;
+    profileAvatar.style.backgroundImage = `url('${result.avatar}')`;
+    console.log(result);
+  });
+}
+
+function patchUser() {
+  fetch('https://nomoreparties.co/v1/wff-cohort-42/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: '4baeca36-0e67-4bf5-a839-8080e6cb541f',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: nameInput.value,
+      about: jobInput.value
+    })
+  });
+}
+
+function postCard() {
+  fetch('https://nomoreparties.co/v1/wff-cohort-42/cards', {
+    method: 'POST',
+    headers: {
+      authorization: '4baeca36-0e67-4bf5-a839-8080e6cb541f',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: cardName.value,
+      link: cardLink.value
+    })
+  });
+}
+
+
+Promise.all([getUser(), getCards()]).then(([userData, cards]) => {
+  
+})
